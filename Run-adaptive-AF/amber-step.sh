92658cc7cbb2545319fbeb1c544e02a35e6c4cc2
sbhakat <33577253+sbhakat@users.noreply.github.com>
2022-03-03 13:55:52 -0600

    Create amber-step.sh

---
+++ Run-adaptive-AF/amber-step.sh
@@ -0,0 +1,8 @@
+for i in {8,9,14,23,26,27,28,30,31}; do
+cp min.in Model-${i}
+cp full-min.in Model-${i}
+cd Model-${i}
+sander -O -i min.in -o min.out -p com_solvated.top -c com_solvated.crd -ref com_solvated.crd -r Partial_Mini.rst
+sander -O -i full-min.in -o full-min.out -p com_solvated.top -c Partial_Mini.rst -r Full_Mini.rst
+ambpdb -p com_solvated.top -c Full_Mini.rst > Full_Mini.pdb
+echo 0 | gmx trjconv -s Full_Mini.pdb -f Full_Mini.pdb -o Full-min.gro
